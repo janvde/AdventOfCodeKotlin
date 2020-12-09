12730490c952818e7a083dfd7f7fb78103c521f1
Jan van den Ende <jan@getroadmap.com>
2020-12-09 23:09:13 +0100

    Initial commit

---
+++ src/main/kotlin/day1/day1.kt
@@ -0,0 +1,49 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val input = readInput(args[0])
+    val result = sumTo3(input, 2020)
+    println(result)
+    println(multiplyList(result))
+}
+
+fun sumTo(list: List<Int>, sum: Int): List<Int> {
+    val set = list.toSet()
+    set.forEach {
+        val remainder = sum - it
+        if (set.contains(remainder)) {
+            return listOf(remainder, it)
+        }
+    }
+    return emptyList()
+}
+
+fun sumTo3(list: List<Int>, sum: Int): List<Int> {
+    list.forEachIndexed { index, item ->
+        val remainder = sum - item
+        val subList = list.subList(0, index)
+        val parts = sumTo(subList, remainder)
+        if(parts.isNotEmpty()){
+            val result = mutableListOf<Int>(item)
+            result.addAll(parts)
+            return result
+        }
+    }
+    return emptyList()
+}
+
+fun multiplyList(list: List<Int>): Int {
+    var result = 1
+    list.forEach {
+        result = result * it
+    }
+    return result
+}
+
+fun readInput(fileName: String): List<Int> {
+    val output = mutableListOf<Int>()
+    File(fileName).bufferedReader().forEachLine {
+        output.add(it.toInt())
+    }
+    return output
+}
