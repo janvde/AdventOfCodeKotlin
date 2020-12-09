7acda9f0b75d9c2c9784deb46ae00c6d00ebd117
Jan van den Ende <jan@getroadmap.com>
2020-12-09 23:48:45 +0100

    day2 part 1

---
+++ src/main/kotlin/day2/Password.kt
@@ -0,0 +1,8 @@
+package day2
+
+data class Password(
+    val minimum: Int,
+    val maximum : Int,
+    val char: String,
+    val code: String
+)
---
+++ src/main/kotlin/day2/day2.kt
@@ -0,0 +1,34 @@
+package day2
+
+import java.io.File
+import java.util.*
+
+fun main(args: Array<String>) {
+    val input = readInput(args[0])
+
+    val result = input.count { password ->
+        val count = password.code.count { it.toString() == password.char }
+        count >= password.minimum && count <= password.maximum
+    }
+
+    println(result)
+}
+
+
+fun readInput(fileName: String): List<Password> {
+    val output = mutableListOf<Password>()
+    File(fileName).bufferedReader().forEachLine {
+        val line = it.replace("-", " ").replace(":", "")
+        val scanner = Scanner(line)
+        val minimum = scanner.nextInt()
+
+        val maximum = scanner.nextInt()
+        val char = scanner.next()
+        val code = scanner.next()
+
+
+        val password = Password(minimum, maximum, char, code)
+        output.add(password)
+    }
+    return output
+}
